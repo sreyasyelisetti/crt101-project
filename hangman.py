0c0ae6795a4ea80814b0bbb3b1814554bb03a87f
Anay Mediwala <anay.m@outlook.com>
2026-07-09 17:15:57 -0700

    Added lives counter, imported random words

--- hangman.py
+++ hangman.py
@@ -1,3 +1,5 @@
+from random_word import RandomWords
+
 print("Let's play Hangman!")
 print("-----")
 
@@ -7,3 +9,10 @@ while playing == True:
 
     for lives in range(6):
         print("Lives left: " + str(6 - lives))
+        guess = input("Guess a letter:\n")
+
+        for letter in guess:
+            if letter in word:
+                print("Correct!")
+            else:
+                print("Incorrect!")
